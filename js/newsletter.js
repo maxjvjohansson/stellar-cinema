e303a24a255678dfd1a9b5fc487936b2aa8063bb
maxjvjohansson <max.jv.johansson@gmail.com>
2024-12-02 22:00:02 +0100

    updated slideshow with indicators that are clickable

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,35 +1,68 @@
-let currentOfferIndex = 1; 
+let currentOfferIndex = 0;
 const totalOffers = 3; 
 
 const offers = document.querySelectorAll('.offer');
 const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
+const indicators = document.querySelectorAll('.indicator');
 
 function showOffer(index) {
     offers.forEach((offer, i) => {
-        offer.style.display = (i === index) ? 'block' : 'none'; 
+        offer.style.display = (i === index) ? 'block' : 'none';
+    });
+    updateIndicators(index);
+}
+
+// Function for circle fill
+function updateIndicators(index) {
+    indicators.forEach((indicator, i) => {
+        if (i === index) {
+            indicator.classList.add('active');
+        } else {
+            indicator.classList.remove('active');
+        }
     });
 }
 
+// Previous offer
 rightArrow.addEventListener('click', () => {
     currentOfferIndex = (currentOfferIndex + 1) % totalOffers;
     showOffer(currentOfferIndex);
+    resetAutoSwitch();
 });
 
+// Next offer
 leftArrow.addEventListener('click', () => {
     currentOfferIndex = (currentOfferIndex - 1 + totalOffers) % totalOffers;
     showOffer(currentOfferIndex);
+    resetAutoSwitch();
 });
 
+// Clickable circles
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', () => {
+        currentOfferIndex = index;
+        showOffer(currentOfferIndex);
+        resetAutoSwitch();
+    });
+});
+
+// Automatic slideshow every 5 seconds
 let autoSwitch = setInterval(() => {
     currentOfferIndex = (currentOfferIndex + 1) % totalOffers;
     showOffer(currentOfferIndex);
 }, 5000);
 
-leftArrow.addEventListener('click', () => {
-    clearInterval(autoSwitch); 
-});
+// Reset automatic slideshow if clicked
+function resetAutoSwitch() {
+    clearInterval(autoSwitch);
+    autoSwitch = setInterval(() => {
+        currentOfferIndex = (currentOfferIndex + 1) % totalOffers;
+        showOffer(currentOfferIndex);
+    }, 5000);
+}
 
-rightArrow.addEventListener('click', () => {
-    clearInterval(autoSwitch); 
-});
+showOffer(currentOfferIndex);
+
+
+
